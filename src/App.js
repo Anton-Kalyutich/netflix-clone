ebec0c11fbcd56c54a3e74e47ae3b9302b59e2e4
Anton Kalyutich <anton.kalyutich@gmail.com>
2024-07-10 17:17:56 -0400

    Initiated Row component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Row title='NETFLIX ORIGINALS' />
+      <Row title='Trending Now' />
     </div>
   );
 }
